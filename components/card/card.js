8b3f96a35de0a4266f0aa6c792874721372d537b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-20 17:48:06 +0200

    "Worked on innerHTML and fetch data"
    Co-authored-by: Kwanele Buthelezi <kwanelebuthelezi2001@gmail.com>

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,34 @@
-export function createCharacterCard() {}
+export function createCharacterCard(character) {
+// we are going to create article elements for card.
+
+const cardElement = document.createElement('article');
+cardElement.classList.add('card');
+
+// setting innerHTML for the card.
+
+cardElement.innerHTML = `
+<figure class = 'card_image-container'>
+< img class ='card_image'
+ src = '${character.image}'
+ alt = '${character.name}' 
+  <figcaption class = 'card__image-gradient'></figcaption>
+ figure/>
+ 
+= <section class 'card__content'>
+<header class = 'card__title'>
+<h2 class ='card__title'></h2>
+</header>
+
+ <dl class="card__info">
+    <dt class="card__info-title"></dt>
+    <dd class="card__info-description"></dd>
+    <dt class="card__info-title"></dt>
+    <dd class="card__info-description"></dd>
+    <dt class="card__info-title"></dt>
+    <dd class="card__info-description"></dd>
+  </dl>
+</section>
+`
+}
+
+createCharacterCard();
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,43 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// we need to write async function to fetch.
+
+async function fetchCharacter () {
+  const response = await fetch(`https://rickandmortyapi.com/api`);
+  const data = await response.json();
+  
+  maxPage = data.info.pages;
+  cardcontainer.innerHTML = '';
+
+  data.result.foreach(Character  => {
+    const card = createCharacterCard ({
+    image: character.image,
+    name: character.name,
+    status: character.status,
+    type: character.type,
+    episodcount: character.episodcount.occurrences
+    });
+    cardcontainer.appendChild(card);
+  });
+
+  /* Great Job! But we want to see not only 20 characters, we want all of them! So 
+  lets implement the pagination.
+
+By adding the string ?page=<pageIndex> to the end of the fetch URL, you can receive 
+the respective page of characters.
+Use here the state variable page to keep track of the current page index.
+Inside of the info part of the received data you can find the max page count.
+Add an event listener on each of the next and prev buttons which do the following
+it is prevented that the page index could go higher than the max page index or below 1
+the page index is increased / decreased
+the fetchCharacters function is called
+Update the pagination display each time characters are fetched to show the 
+current page index and the current max page index.*/
+
+pagination.textContent = `${page} / ${maxPage}`;
+
+}
+
+fetchCharacter();
