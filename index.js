33841e3bcebd750c0a5126288a7943d212ea50fa
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-21 11:41:10 +0200

    changed fetch url

--- index.js
+++ index.js
@@ -16,42 +16,46 @@ let page = 1;
 let searchQuery = "";
 
 // we need to write async function to fetch.
-
-async function fetchCharacter () {
-  const response = await fetch(`https://rickandmortyapi.com/api`);
-  const data = await response.json();
   
-  maxPage = data.info.pages;
-  pagination.textContent = `${page} / ${maxPage}`;
+      async function fetchCharacter () {
+        try { const response = await fetch( `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`);
+          const data = await response.json();
+          
+          maxPage = data.info.pages;
+          pagination.textContent = `${page} / ${maxPage}`;
+  
+          cardContainer.innerHTML = '';
+  
+          data.results.forEach(character  => {
+            const card = createCharacterCard ({
+            image: character.image,
+            name: character.name,
+            status: character.status,
+            type: character.type || 'Em',
+            episodeCount: character.episode.length,
+            });
+            cardContainer.appendChild(card);
+          });}
+       
 
-  cardContainer.innerHTML = '';
+        /* Great Job! But we want to see not only 20 characters, we want all of them! So 
+        lets implement the pagination.
 
-  data.result.foreach(character  => {
-    const card = createCharacterCard ({
-    image: character.image,
-    name: character.name,
-    status: character.status,
-    type: character.type,
-    episodcount: character.episodcount.occurrences
-    });
-    cardContainer.appendChild(card);
-  });
+      By adding the string ?page=<pageIndex> to the end of the fetch URL, you can receive 
+      the respective page of characters.
+      Use here the state variable page to keep track of the current page index.
+      Inside of the info part of the received data you can find the max page count.
+      Add an event listener on each of the next and prev buttons which do the following
+      it is prevented that the page index could go higher than the max page index or below 1
+      the page index is increased / decreased
+      the fetchCharacters function is called
+      Update the pagination display each time characters are fetched to show the 
+      current page index and the current max page index.*/
+       catch (error) {
+      console.log('Fetch error occured', error)
+       };
 
-  /* Great Job! But we want to see not only 20 characters, we want all of them! So 
-  lets implement the pagination.
-
-By adding the string ?page=<pageIndex> to the end of the fetch URL, you can receive 
-the respective page of characters.
-Use here the state variable page to keep track of the current page index.
-Inside of the info part of the received data you can find the max page count.
-Add an event listener on each of the next and prev buttons which do the following
-it is prevented that the page index could go higher than the max page index or below 1
-the page index is increased / decreased
-the fetchCharacters function is called
-Update the pagination display each time characters are fetched to show the 
-current page index and the current max page index.*/
-
-}
+    }
 
 fetchCharacter();
 
@@ -87,7 +91,7 @@ index might have to change when you start searching for only subsets of all char
 
 searchBar.addEventListener('submit', (event) => {
 event.preventDefault();
-searchQuery = searchBar.target.query.value.trim();
+searchQuery = event.target.query.value.trim();
 page = 1;
 fetchCharacter();
 });
