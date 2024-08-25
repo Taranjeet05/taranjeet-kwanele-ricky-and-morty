5b3de729782423b8a618bcd6b4be6642bd6163a9
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-25 18:41:51 +0200

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 
-// States
+
 let maxPage = 1;
 let page = 1;
 let searchQuery = "";
